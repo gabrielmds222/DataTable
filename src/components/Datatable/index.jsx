409a2eef749471eaeaea8a4faf8aa86d17f00cc1
gabrielmds222 <gabrielmedsilva@outlook.com>
2023-01-05 22:42:50 -0300

    feat: Código bootstrap de teste

--- src/components/Datatable/index.jsx
+++ src/components/Datatable/index.jsx
@@ -2,8 +2,17 @@ import React from "react";
 
 const Datatable = () => {
   return (
-    <div>
-      <h1>Datatable</h1>
+    <div className="container text-center">
+      <div className="row">
+        <div className="col">1 of 3</div>
+        <div className="col-6">2 of 3 (wider)</div>
+        <div className="col">3 of 3</div>
+      </div>
+      <div className="row">
+        <div className="col">1 of 3</div>
+        <div className="col-5">2 of 3 (wider)</div>
+        <div className="col">3 of 3</div>
+      </div>
     </div>
   );
 };
