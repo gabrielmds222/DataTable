c61e2c0abd615b313f9c8708999c0d535ef8ccd3
gabrielmds222 <gabrielmedsilva@outlook.com>
2023-01-05 22:54:32 -0300

    feat: Adiciona layout da tela home

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from "react";
 import "./App.css";
-import Datatable from "./components/Datatable";
+// import Datatable from "./components/Datatable";
 
 function App() {
-  return <Datatable />;
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-sm-12">
+          <h1>Header</h1>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-sm-4">
+          <h1>Sidebar</h1>
+        </div>
+      </div>
+      <div className="col-sm-8">
+        <h1>Datatable</h1>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Datatable/index.jsx
+++ src/components/Datatable/index.jsx
@@ -2,17 +2,8 @@ import React from "react";
 
 const Datatable = () => {
   return (
-    <div className="container text-center">
-      <div className="row">
-        <div className="col">1 of 3</div>
-        <div className="col-6">2 of 3 (wider)</div>
-        <div className="col">3 of 3</div>
-      </div>
-      <div className="row">
-        <div className="col">1 of 3</div>
-        <div className="col-5">2 of 3 (wider)</div>
-        <div className="col">3 of 3</div>
-      </div>
+    <div>
+      <h1>DataTable</h1>
     </div>
   );
 };
