c7966c9cdc128e35b38a4dc8aed5c04c57b5fad9
gabrielmds222 <gabrielmedsilva@outlook.com>
2023-01-16 15:41:48 -0300

    feat: Cria páginas do projeto

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Datatable from "../../components/Datatable";
+import "./style.css";
+
+const Home = () => {
+  return (
+    <>
+      <Datatable />
+    </>
+  );
+};
+
+export default Home;
